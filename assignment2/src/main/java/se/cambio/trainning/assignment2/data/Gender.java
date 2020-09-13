73355084e9df567a170960d79a3625664f2f3520
Chandima Niroshan <cniroshan3@gmail.com>
2020-09-13 09:29:00 +0530

    initial commit - assignment2

---
+++ assignment2/src/main/java/se/cambio/trainning/assignment2/data/Gender.java
@@ -0,0 +1,16 @@
+package se.cambio.trainning.assignment2.data;
+
+public enum Gender {
+	Male(0), Female(1);
+
+	private int value;
+
+	private Gender(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+}
